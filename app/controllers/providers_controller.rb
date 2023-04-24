8f7479c47290dd44b37aba6c5e377a6b5b633c7f
Heber Lazo <heberlazo@gmail.com>
2023-04-24 16:59:06 -0500

    Added provider cru

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,2 +1,34 @@
 class ProvidersController < ApplicationController
+  before_action :set_provider, only: [:edit, :update]
+  def index
+    @provider = Provider.new
+    @providers = Provider.all
+  end
+
+  def edit;end
+
+  def update
+    if @provider.update(providers_params)
+      redirect_to providers_path
+    end
+  end
+
+  def create
+    @provider = provider.new(providers_params)
+
+    if @provider.save
+
+    end
+
+  end
+
+
+
+  def set_provider
+    @provider = Provider.find(params[:id])
+  end
+
+  def providers_params
+    params.require(:provider).permit(:name)
+  end
 end
