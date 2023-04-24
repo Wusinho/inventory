830a972773d9d3aca79b173fa854c928282d3c33
Heber Lazo <heberlazo@gmail.com>
2023-04-24 17:21:56 -0500

    Product CRU

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,34 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:edit, :update]
+  def index
+    @product = Product.new
+    @products = Product.all
+  end
+
+  def edit;end
+
+  def update
+    if @product.update(products_params)
+      redirect_to products_path
+    end
+  end
+
+  def create
+    @product = Product.new(products_params)
+
+    if @product.save
+
+    end
+
+  end
+
+
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def products_params
+    params.require(:product).permit(:name, :description, :provider_id)
+  end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,8 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def providers_select
+    Provider.pluck(:name, :id)
+  end
 end
