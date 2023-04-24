faedc55cdcdf78cd1c377deb1df53cb5c177e481
Heber Lazo <heberlazo@gmail.com>
2023-04-24 13:31:41 -0500

    refactor seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+if Rails.env.development?
+  Admin.create(email: 'user_0@gmail.com',
+               password: '123456',
+               password_confirmation: '123456',
+               name: 'user', last_name: 'last_user')
+end
