04e4c06f1d544fb4982a230720687704529673a5
Heber Lazo <heberlazo@gmail.com>
2023-04-24 13:45:25 -0500

    Added category CRU

--- Gemfile
+++ Gemfile
@@ -78,3 +78,4 @@ end
 
 gem 'devise', '~> 4.8', '>= 4.8.1'
 gem 'bootstrap', '~> 5.1.3'
+gem "font-awesome-sass", "~> 6.3.0"
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,35 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:edit, :update]
   def index
+    @category = Category.new
+    @categories = Category.all
+  end
+
+  def edit;end
+
+  def update
+    if @category.update(categories_params)
+      redirect_to categories_path
+    end
   end
 
   def create
+    @category = Category.new(categories_params)
+
+    if @category.save
+
+    end
+
   end
+
+
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def categories_params
+    params.require(:category).permit(:name)
+  end
+
 end
