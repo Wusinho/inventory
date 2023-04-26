2b11ccd8a274c1977474892a70d00c3b6385eb3b
Heber Lazo <heberlazo@gmail.com>
2023-04-26 13:23:57 -0500

    removed sales buttons if there is not stock

--- app/models/inventory_purchase.rb
+++ app/models/inventory_purchase.rb
@@ -2,4 +2,9 @@ class InventoryPurchase < ApplicationRecord
   belongs_to :product
   has_many :selling_prices
   accepts_nested_attributes_for :selling_prices, reject_if: :all_blank, allow_destroy: true
+
+  def stock?
+    self.stock_quantity.positive?
+  end
+
 end
