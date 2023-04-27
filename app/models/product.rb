0e23afa6f4c214c8ea55146b1de51d73eca76184
Heber Lazo <heberlazo@gmail.com>
2023-04-26 23:12:28 -0500

    Added validation for tag

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,16 @@
 class Product < ApplicationRecord
   belongs_to :provider
   has_many :inventory_purchases
-  acts_as_taggable_on :tags
   accepts_nested_attributes_for :inventory_purchases, allow_destroy: true
   validates_presence_of :name
+  validate :tag_list_empty
+  acts_as_taggable_on :tags
   after_create_commit { broadcast_prepend_to "products" }
+
+  def tag_list_empty
+    return unless tag_list.blank?
+
+    errors.add(:tag_list, 'Categories cannot be empty')
+  end
 
 end
