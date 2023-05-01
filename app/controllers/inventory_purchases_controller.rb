3e1a88517e6f72ef8814bdb24d048834d18a7ba6
Heber Lazo <heberlazo@gmail.com>
2023-05-01 18:45:40 -0500

    refactor controller

--- app/controllers/inventory_purchases_controller.rb
+++ app/controllers/inventory_purchases_controller.rb
@@ -1,13 +1,13 @@
 class InventoryPurchasesController < ApplicationController
   before_action :authenticate_admin!
   before_action :set_inventory_purchases, only: [:edit, :update]
-  before_action :product_builder, only: [:create]
 
   def edit;end
 
   def create
     @inventory_purchase = InventoryPurchase.new(inventory_purchases_params)
     if @inventory_purchase.save
+      product_builder
       streams = []
       streams <<  turbo_stream.prepend("inventory_purchases", partial: 'inventory_purchases/inventory_purchase', locals: { inventory_purchase: @inventory_purchase })
       streams <<  turbo_stream.replace("new_purchase", partial: 'inventory_purchases/simple_form', locals: { inventory_purchase: @new_inventory_purchase })
@@ -36,10 +36,6 @@ class InventoryPurchasesController < ApplicationController
     product = Product.find(params[:inventory_purchase][:product_id])
     @new_inventory_purchase = product.inventory_purchases.build
   end
-
-  # def selling_inventory_purchases_params
-  #   params.require(:cost).permit(selling_prices_attributes: [:id, :cost, :quantity, :special_cost, :_destroy])
-  # end
 
   def inventory_purchases_params
     params.require(:inventory_purchase).permit(:product_id, :stock_quantity, :purchase_price, :sold_out, :selling_price)
