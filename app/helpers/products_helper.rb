df01ffdd69d1ae1ba280d80094293e7942dd90ae
Heber Lazo <heberlazo@gmail.com>
2023-05-02 19:08:32 -0500

    removed condition from view

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,4 +5,15 @@ module ProductsHelper
 
     render 'inventory_purchases/form', f: f
   end
+
+    def product_categories_select(product, f)
+      options = { include_blank: true, multiple: true, class: 'form-select', id: 'validationTagsThreshold', data: { controller: 'tag-select', allow_clear: true, suggestions_threshold: 0 } }
+
+      if product.persisted?
+        f.collection_select(:product_categories_attributes, product.all_categories, :id, :name, { include_blank: true, selected: product.category_ids }, options )
+        else
+        f.collection_select(:product_categories_attributes, product.all_categories, :id, :name, { include_blank: true }, options)
+      end
+    end
+
 end
