8b738dd2199b95749c75b8dff1855e59c8f275db
Heber Lazo <heberlazo@gmail.com>
2023-05-02 20:07:23 -0500

    corrected missing strong params in inventory purchase controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :provider
-  has_many :inventory_purchases
+  has_many :inventory_purchases, -> { order(sold_out: :asc, created_at: :asc) }, dependent: :delete_all
   has_many :product_categories
   has_many :categories, through: :product_categories
   accepts_nested_attributes_for :inventory_purchases, allow_destroy: true
