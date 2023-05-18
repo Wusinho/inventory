1a82a6594637b71a936220f5474facb73b13ac1c
Heber Lazo <heberlazo@gmail.com>
2023-05-18 12:27:13 -0500

    improved form toggle

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,36 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form"
 export default class extends Controller {
   static targets = ['dropdownContent', 'btn']
-  static values = { open: Boolean }
   connect() {
     // this.toggle(this.openValue)
   }
 
-  toggle(value){
-    if (value === true) {
-      this.openDropdown()
-    } else {
-      this.closeDropdown()
-    }
-  }
-
-  openDropdown() {
-    try {
-      this.dropdownContentTarget.classList.remove('d-none')
-    }
-    catch {
-    }
-  }
-
-  closeDropdown() {
-    try{
-      this.dropdownContentTarget.classList.add('d-none')
-    }catch {}
-  }
-
   toggle_btn(e){
     e.preventDefault()
-    this.openValue = !this.openValue
-    this.toggle(this.openValue)
+
+  const form = this.dropdownContentTarget
+  form.classList.length > 0 ?
+      form.classList.remove('d-none') :
+      form.classList.add('d-none')
+
   }
 }
