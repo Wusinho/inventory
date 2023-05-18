92e30f569716a3faf3f8c2c097b9ba0bba82712d
Heber Lazo <heberlazo@gmail.com>
2023-05-18 13:31:54 -0500

    uncomit code

--- db/seeds.rb
+++ db/seeds.rb
@@ -60,45 +60,45 @@ product_categories = [
   "suplementos",
   "aromaterapia"
 ]
-# product_categories.each { |cat_name| Category.create(name: cat_name)}
+product_categories.each { |cat_name| Category.create(name: cat_name)}
 
 if Rails.env.development?
   Admin.create(email: 'user_0@gmail.com',
                password: '123456',
                password_confirmation: '123456',
                name: Faker::Name.first_name , last_name: Faker::Name.middle_name  )
-  # 5.times do
-  #   Provider.create(name: Faker::Company.name,
-  #                   phone: Faker::PhoneNumber.cell_phone,
-  #                   address: Faker::Address.full_address,
-  #                   contact: Faker::Name.name,
-  #                   )
-  #   Customer.create(name: Faker::Name.first_name ,
-  #                   last_name: Faker::Name.last_name,
-  #                   phone: Faker::PhoneNumber.cell_phone)
-  # end
-  # Provider.all.each do |provider|
-  #   [1,2].sample.times do
-  #     cat_ids = Category.all.sample([1,2,3].sample)
-  #
-  #     product = Product.create(name: Faker::Commerce.product_name,
-  #                             provider_id: provider.id,
-  #                             description: Faker::Lorem.paragraph,
-  #                             )
-  #
-  #     cat_ids.each { |cat| ProductCategory.create(product_id: product.id, category_id: cat.id ) }
-  #
-  #   end
-  # end
+  5.times do
+    Provider.create(name: Faker::Company.name,
+                    phone: Faker::PhoneNumber.cell_phone,
+                    address: Faker::Address.full_address,
+                    contact: Faker::Name.name,
+                    )
+    Customer.create(name: Faker::Name.first_name ,
+                    last_name: Faker::Name.last_name,
+                    phone: Faker::PhoneNumber.cell_phone)
+  end
+  Provider.all.each do |provider|
+    [1,2].sample.times do
+      cat_ids = Category.all.sample([1,2,3].sample)
 
-  # Product.all.each do |product|
-  #   [4,6,8].sample.times {
-  #     price = Faker::Commerce.price
-  #     i = InventoryPurchase.create(product_id: product.id,
-  #                                  purchase_price: price,
-  #                                  stock_quantity: [4,5].sample,
-  #                                  selling_price: price * 1.2)
-  #   }
-  # end
+      product = Product.create(name: Faker::Commerce.product_name,
+                              provider_id: provider.id,
+                              description: Faker::Lorem.paragraph,
+                              )
+
+      cat_ids.each { |cat| ProductCategory.create(product_id: product.id, category_id: cat.id ) }
+
+    end
+  end
+
+  Product.all.each do |product|
+    [4,6,8].sample.times {
+      price = Faker::Commerce.price
+      i = InventoryPurchase.create(product_id: product.id,
+                                   purchase_price: price,
+                                   stock_quantity: [4,5].sample,
+                                   selling_price: price * 1.2)
+    }
+  end
 
 end
