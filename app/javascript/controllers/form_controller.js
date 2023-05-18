057b886f1808477ceed2d1db41e9e50243374fd1
Heber Lazo <heberlazo@gmail.com>
2023-05-18 13:29:14 -0500

    refactor toggle form

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,8 +3,18 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form"
 export default class extends Controller {
   static targets = ['dropdownContent', 'btn']
+
   connect() {
-    // this.toggle(this.openValue)
+  }
+
+  openDropdown(form) {
+      form.classList.remove('d-none')
+    this.btnTarget.innerText = this.add_new(this.btnTarget.innerText)
+  }
+
+  closeDropdown(form) {
+      form.classList.add('d-none')
+    this.btnTarget.innerText = this.hide_btn(this.btnTarget.innerText)
   }
 
   toggle_btn(e){
@@ -12,8 +22,16 @@ export default class extends Controller {
 
   const form = this.dropdownContentTarget
   form.classList.length > 0 ?
-      form.classList.remove('d-none') :
-      form.classList.add('d-none')
+      this.openDropdown(form) :
+      this.closeDropdown(form)
+  }
 
+  add_new(str) {
+    return str.replace('Add new', 'Hide');
+  }
+
+  hide_btn(str){
+    return str.replace('Hide ', 'Add new ');
   }
 }
+
