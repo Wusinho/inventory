307d003b93815cfb0c12baf9ffdc7bc8229a043a
Heber Lazo <heberlazo@gmail.com>
2023-05-26 19:00:19 -0500

    refactor condition in prod colors

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -27,7 +27,8 @@ module ProductsHelper
   def faulty_prod_card(product)
     css = 'card'
     css += ' border-danger border-3' if product.faulty
-    css += product.out_of_stock? ? ' border-warning' : ' border-primary'
+    css += ' border-warning' unless product.out_of_stock?
+    css
   end
 
   def product_form(product)
