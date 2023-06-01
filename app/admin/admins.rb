81ecd22b2a30f5614ca013c5d62b3193d7adb9c5
Heber Lazo <heberlazo@gmail.com>
2023-06-01 18:07:26 -0500

    admin panel

--- app/admin/admins.rb
+++ app/admin/admins.rb
@@ -15,6 +15,16 @@ ActiveAdmin.register Admin do
   #   permitted
   # end
 
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    actions
+  end
+
   form do |f|
     f.inputs do
       f.input :email
