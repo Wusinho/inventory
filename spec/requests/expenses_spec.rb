9b8c27f6a1d54f02d8616c66a2b57daa685295e1
Heber Lazo <heberlazo@gmail.com>
2023-06-06 11:37:49 -0500

    fixed refactor model naming

--- spec/requests/expenses_spec.rb
+++ spec/requests/expenses_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Expenses", type: :request do
+RSpec.describe "expenses", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
